2d782979a4c185d15680fcf078cd650a52dbee85
Agus Tinus Turnip <tinusagus60@gmail.com>
2025-06-06 11:18:24 +0700

    feat(service): add member service

---
+++ src/main/java/me/agus/bookstore/services/MemberService.java
@@ -0,0 +1,36 @@
+package me.agus.bookstore.services;
+
+import me.agus.bookstore.models.Member;
+import me.agus.bookstore.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> getMemberById(Long id) {
+        return memberRepository.findById(id);
+    }
+
+    public Member saveMember(Member member) {
+        return memberRepository.save(member);
+    }
+
+    public void deleteMember(Long id) {
+        memberRepository.deleteById(id);
+    }
+}
