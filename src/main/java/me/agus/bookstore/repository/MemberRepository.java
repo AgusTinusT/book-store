2767aa4094d8b9c5a7ade027893dda2f54a9e9e1
Agus Tinus Turnip <tinusagus60@gmail.com>
2025-06-06 11:17:01 +0700

    feat(repository): add member repository

---
+++ src/main/java/me/agus/bookstore/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package me.agus.bookstore.repository;
+
+import me.agus.bookstore.models.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
