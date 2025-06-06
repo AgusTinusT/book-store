f6f06da1fbfeba3ed697e760c184dadbc84fab92
Agus Tinus Turnip <tinusagus60@gmail.com>
2025-06-06 10:52:29 +0700

    feat(enum): add transaction type and status

---
+++ src/main/java/me/agus/bookstore/models/enums/TransactionStatus.java
@@ -0,0 +1,8 @@
+package me.agus.bookstore.models.enums;
+
+public enum TransactionStatus {
+    SELESAI,
+    TERTUNDA,
+    TERLAMBAT,
+    BATAL
+}
---
+++ src/main/java/me/agus/bookstore/models/enums/TransactionType.java
@@ -0,0 +1,7 @@
+package me.agus.bookstore.models.enums;
+
+public enum TransactionType {
+    PEMBELIAN,
+    PEMINJAMAN,
+    PENGEMBALIAN,
+}
