34f4c34b8515210974feb0fc17d97bfd6440b7b2
Agus Tinus Turnip <tinusagus60@gmail.com>
2025-06-06 11:16:49 +0700

    feat(repository): add book repository

---
+++ src/main/java/me/agus/bookstore/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package me.agus.bookstore.repository;
+
+import me.agus.bookstore.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
